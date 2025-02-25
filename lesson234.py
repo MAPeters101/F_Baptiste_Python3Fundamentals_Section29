938bbefcac418c3ee7851c91aa3ed5ee882b6dcf
Mark Peters <mapeters101@earthlink.net>
2025-02-24 19:46:57 -0500

    Work on lesson234 from 6:45 to 9:20.

--- lesson234.py
+++ lesson234.py
@@ -20,7 +20,26 @@ a = np.array([1,2,3,255], dtype=np.uint8)
 print(a)
 #a = np.array([1,2,3,256], dtype=np.uint8)
 #print(a)
-a = np.array([127], dtype=np.int8)
+#a = np.array([127], dtype=np.int8)
 print(a)
 #a = np.array([128], dtype=np.int8)
 #print(a)
+print('-'*80)
+
+a = np.array([1,2,3.14])
+print(a)
+print(a.dtype)
+
+a = np.array([1,2,9.9, 9.1], dtype=np.int64)
+print(a)
+print(a.dtype)
+
+a = np.array([1, 3.14, 'x'])
+print(a)
+print(a.dtype)
+
+#a = np.array([1, 3.14, 'x'], dtype=np.int64)
+#print(a)
+#print(a.dtype)
+print('-'*80)
+
