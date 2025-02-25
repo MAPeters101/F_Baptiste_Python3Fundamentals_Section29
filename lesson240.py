f7f8e5aa7a45e539fb6f1e276caff1a9aabe9c92
Mark Peters <mapeters101@earthlink.net>
2025-02-25 14:27:48 -0500

    Work on lesson240 from 2:20 to 5:00.

--- lesson240.py
+++ lesson240.py
@@ -12,3 +12,28 @@ try:
     np.vstack((np.arange(3), a2))
 except ValueError as ex:
     print(ex)
+print('-'*80)
+
+a1 = np.array([1,2,3,4])
+a2 = np.array([0.1,0.2,0.3,0.4])
+result = np.vstack((a1, a2))
+print(a1)
+print(a2)
+print()
+print(result)
+print(result.dtype)
+print('-'*80)
+
+a1 = np.array([1,2,3,4], dtype=np.uint8)
+a2 = np.array([1000,2000,3000,4000], dtype=np.uint16)
+result = np.vstack((a1, a2))
+print(a1)
+print(a2)
+print()
+print(result)
+print(result.dtype)
+print('-'*80)
+
+
+
+
