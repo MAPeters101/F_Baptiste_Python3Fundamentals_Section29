d4e94e48932e94dd311edd84ed356ad06739dae9
Mark Peters <mapeters101@earthlink.net>
2025-02-25 09:37:04 -0500

    Work on lesson236 from 2:28 to 5:20.

--- lesson236.py
+++ lesson236.py
@@ -16,9 +16,32 @@ print()
 a = np.zeros((4, 3), dtype=np.uint8)
 print(a)
 print(a.dtype)
+print('-'*80)
+
+m = np.ones((10,2), dtype=float)
+print(m)
+print(m.dtype)
 print()
 
+m = np.full((2,5), 3.14, dtype=np.float32)
+print(m)
+print(m.dtype)
+print()
 
+m = np.eye(5)
+print(m)
+print(m.dtype)
+print()
+
+m = np.eye(5, dtype=int)
+print(m)
+print(m.dtype)
+print()
+
+m = np.eye(5, 3, dtype=np.uint16)
+print(m)
+print(m.dtype)
+print()
 
 
 
