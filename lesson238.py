1ba0c719d5b76542ce0117b35ff365975c4ee55d
Mark Peters <mapeters101@earthlink.net>
2025-02-25 10:20:33 -0500

    Work on lesson238 to 4:10.

---
+++ lesson238.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+arr = np.arange(12)
+print(arr)
+print(arr.shape)
+m1 = arr.reshape(4,3)
+print(m1)
+m2 = arr.reshape(2,6)
+print(m2)
+m3 = m2.reshape(6,2)
+print(m3)
+print()
+print(arr is m1)
+print(m1 is m2)
+print(m2 is m3)
+print()
+
+print(arr)
+print(m1)
+print(m2)
+arr[0] = 100
+print(arr)
+print(m1)
+print(m2)
+print(m3)
+
+
+
+
