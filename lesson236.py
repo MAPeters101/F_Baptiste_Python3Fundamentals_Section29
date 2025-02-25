2a65a1a6a2ab1ea3cf9e5b48cbc3dd96448758a4
Mark Peters <mapeters101@earthlink.net>
2025-02-25 09:30:29 -0500

    Work on lesson236 to 2:28.

---
+++ lesson236.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.zeros(5)
+print(a)
+print(a.dtype)
+
+a = np.zeros(5, dtype=int)
+print(a)
+print(a.dtype)
+
+a = np.zeros(5, dtype=float)
+print(a)
+print(a.dtype)
+print()
+
+a = np.zeros((4, 3), dtype=np.uint8)
+print(a)
+print(a.dtype)
+print()
+
+
+
+
+
