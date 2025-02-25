f4d7ae069919983d68e7ce1048c7a38efba584a3
Mark Peters <mapeters101@earthlink.net>
2025-02-25 10:31:19 -0500

    Work on lesson238 from 7:00 to 8:20.

--- lesson238.py
+++ lesson238.py
@@ -32,6 +32,39 @@ print(m2)
 print(m3)
 print('-'*80)
 
+m3 = arr.reshape(3,4).copy()
+print(m3)
+print()
+m3[0][0] = 0
+print(m3)
+print(arr)
+print()
+
+m3 = arr.copy().reshape(3,4)
+print(m3)
+print()
+m3[0][0] = 0
+print(m3)
+print(arr)
+print()
+print('-'*80)
+
+m = np.array(
+    [
+        [1,2,3],
+        [4,5,6]
+    ]
+)
+print(m.shape)
+print(m)
+arr2 = m.reshape((6,))
+print(arr2)
+arr2 = m.reshape(6)
+print(arr2)
+arr2[2] = 30
+print(arr2)
+print(m)
+print('-'*80)
 
 
 
