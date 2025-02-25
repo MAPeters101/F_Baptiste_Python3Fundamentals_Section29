29bd8a1ed15a5cabc6f20c2ef1aa3d471a1c7829
Mark Peters <mapeters101@earthlink.net>
2025-02-25 09:46:45 -0500

    Work on lesson236 from 8:10 to 10:15.

--- lesson236.py
+++ lesson236.py
@@ -41,7 +41,19 @@ print()
 m = np.eye(5, 3, dtype=np.uint16)
 print(m)
 print(m.dtype)
+print('-'*80)
+
+print(list(range(2,11,2)))
+print(np.arange(2,11,2))
+print(np.arange(2,11,2, dtype=np.uint8))
+print(np.linspace(2,10,num=5))
+print(np.linspace(2,10,5))
+print(np.linspace(2,10,10))
+print('-'*80)
+
+import math
+x_coords = np.linspace(-2*math.pi, 2*math.pi, 50)
+print(x_coords)
+y_values = np.array([math.sin(x) for x in x_coords])
+print(y_values)
 print()
-
-
-
