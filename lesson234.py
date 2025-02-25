7b57827fa979d01c49b893b50892e57906a08093
Mark Peters <mapeters101@earthlink.net>
2025-02-24 21:39:05 -0500

    Complete work on lesson234 from 9:20.

--- lesson234.py
+++ lesson234.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 a1 = np.array([1,2,3,4])
 a2 = np.array((0.1,0.2,0.3,0.4))
 print(a1)
@@ -42,4 +43,52 @@ print(a.dtype)
 #print(a)
 #print(a.dtype)
 print('-'*80)
+
+
+m_py = [
+    [1,0,0],
+    [0,1,0],
+    [0,0,1]
+]
+print(m_py)
+
+m_py2 = [
+    [1,0,0],
+    [0,1],
+    [0,0,1,2]
+]
+print(m_py2)
+
+m1 = np.array(m_py, dtype=np.int16)
+print(m1)
+print(m1.dtype)
+#m2 = np.array(m_py2, dtype=np.int16)
+#print(m2)
+print('-'*80)
+
+
+print(len(m_py))
+print()
+
+
+a = np.array([1,2,3])
+m2 = np.array(
+    [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [10,11,12]
+    ]
+)
+print(a.size)
+print(m2.size)
+print(m1.size)
+print()
+print(a.shape)
+print(m2.shape)
+print(m1.shape)
+
+
+
+
 
